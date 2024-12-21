883138b0dfb028ec412e9d476fd550eefd022217
Marsel <amn132313@gmail.com>
2024-12-21 14:55:22 +0500

    Реализация требований 5 спринта (#2)
    * реализация требований 5 спринта
    
    * переместил два файла обратно в корень
    
    * поправил код
    
    ---------
    
    Co-authored-by: mnabdrakhmanov <mnabdrakhmanov@avito.ru>

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,15 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by mnabdrakhmanov on 19.12.2024.
+//
+
+import Foundation
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let realRating: Float
+    let correctAnswer: Bool
+}
---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by mnabdrakhmanov on 19.12.2024.
+//
+
+import Foundation
+
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
